c39cfa34e77e067733daabe894781e5142f778ee
carrotisorange <thepropertymanager2020@gmail.com>
2020-11-26 17:21:39 +0800

    asd

--- resources/views/webapp/owners/edit-investor.blade.php
+++ resources/views/webapp/owners/edit-investor.blade.php
@@ -156,7 +156,7 @@
 @section('upper-content')
 <div class="row align-items-center py-4">
   <div class="col-lg-6 col-7">
-    <h6 class="h2 text-dark d-inline-block mb-0">Owner Information</h6>
+    <h6 class="h2 text-dark d-inline-block mb-0">{{ $owner->unit_owner }}</h6>
     
   </div>
 
