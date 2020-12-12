c45f71e5310c9184708ce892603b02db80f9f01e
carrotisorange <thepropertymanager2020@gmail.com>
2020-12-12 21:41:11 +0800

    added emails

--- resources/views/webapp/users/welcome-generated-mail.blade.php
+++ resources/views/webapp/users/welcome-generated-mail.blade.php
@@ -5,7 +5,15 @@
 
 Thanks for signing up to manage your property. During your free trial, you can use all the features of The Property Manager. 
 Should you need help with setting up your property or with using any of the features of the application, 
-feel free to send us a message through the messenger chatbot, located at the bottom left corner, available at any given time. 
+feel free to send us a message through the messenger chatbot, located at the bottom left corner, available 
+at any given time or email us at customercare@thepropertymanager.online. 
+
+<br>
+<br>
+Email: jamesdavid_11@yahoo.com
+<br>
+Password: 12345678
+
 
 <br>
 @component('mail::button', ['url' => 'thepropertymanager.online/property/all'])
