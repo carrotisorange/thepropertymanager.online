f413f3437d62b33d4be066c0b01159854bdb3f16
carrotisorange <thepropertymanager2020@gmail.com>
2021-01-04 11:07:11 +0800

    active users added in dashboard

--- resources/views/layouts/dev/dashboard.blade.php
+++ resources/views/layouts/dev/dashboard.blade.php
@@ -146,7 +146,7 @@
         <div class="card-body">
           <div class="row">
             <div class="col">
-              <h5 class="card-title text-uppercase text-muted mb-0"> Users</h5>
+              <h5 class="card-title text-uppercase text-muted mb-0"> Active users</h5>
               <span class="h2 font-weight-bold mb-0">{{ number_format($active_users->count(),0) }}</span>
             </div>
             <div class="col-auto">
