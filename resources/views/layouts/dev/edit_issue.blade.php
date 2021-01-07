65fbb6104be0a08461808ca85fb38f0404db06ba
carrotisorange <thepropertymanager2020@gmail.com>
2021-01-07 13:23:49 +0800

    improved concerns page.

--- resources/views/layouts/dev/edit_issue.blade.php
+++ resources/views/layouts/dev/edit_issue.blade.php
@@ -148,14 +148,7 @@
         </div>  
 </form>
 <hr>
-<div class="row align-items-center py-4">
-  <div class="col-lg-6 col-7">
-    <h6 class="h2 text-dark d-inline-block mb-0">Responses </h6>
-    
-  </div>
-  
 
-</div>
 
 <div class="row">
   <div class="col">
