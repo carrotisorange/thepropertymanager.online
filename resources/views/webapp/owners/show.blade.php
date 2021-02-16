d4b1e44149062407bdcda2a48a4b910ccaca95c1
= <=>
2021-02-16 09:43:17 +0800

    adds portforlio layout.

--- resources/views/webapp/owners/show.blade.php
+++ resources/views/webapp/owners/show.blade.php
@@ -323,7 +323,7 @@
   </div>
 
 
-<div class="modal fade" id="userAccess" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="userAccess" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"  data-backdrop="static" data-keyboard="false">
   <div class="modal-dialog modal-md" role="document">
   <div class="modal-content">
     <div class="modal-header">
