94a5a4322189d9a15a1a4b732358bcbe42374d8c
= <=>
2021-02-16 08:51:16 +0800

    refactors code in search view.

--- resources/views/webapp/properties/search.blade.php
+++ resources/views/webapp/properties/search.blade.php
@@ -5,7 +5,7 @@
 @section('upper-content')
 <div class="row align-items-center py-4">
   <div class="col-lg-6 col-7">
-    <h6 class="h2 text-dark d-inline-block mb-0"><span class=""> <small> you searched for </small></span> <span class="text-danger">"{{ $search_key }}"<span></h6>
+    <h6 class="h2 text-dark d-inline-block mb-0"><span class=""> <small> You searched for </small></span> <span class="text-danger">"{{ $search_key }}"<span></h6>
   </div>
 
 </div>
