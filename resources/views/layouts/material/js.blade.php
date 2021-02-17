c8d479e3ac76c7a98df994d85e1571a7aa3251b6
= <=>
2021-02-17 08:47:43 +0800

    fixes issue with dev dashboar.d

--- resources/views/layouts/material/js.blade.php
+++ resources/views/layouts/material/js.blade.php
@@ -32,13 +32,15 @@
  <!-- Library for adding dinamically elements -->
  <script src="{{ asset('/material/assets/js/plugins/arrive.min.js') }}"></script>
  <!--  Google Maps Plugin    -->
- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+ {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> --}}
  <!-- Chartist JS -->
  <script src="{{ asset('/material/assets/js/plugins/chartist.min.js') }}"></script>
  <!--  Notifications Plugin    -->
  <script src="{{ asset('/material/assets/js/plugins/bootstrap-notify.js') }}"></script>
  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
- <script src=".{{ asset('/material/assets/js/material-dashboard.js?v=2.1.2') }}" type="text/javascript"></script>
+ <script src="{{ asset('/material/assets/js/material-dashboard.js?v=2.1.2') }}" type="text/javascript"></script>
  <!-- Material Dashboard DEMO methods, don't include it in your project! -->
  <script src="{{ asset('/material/assets/demo/demo.js') }}"></script>
+ <script src="{{ asset('/argon/assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
+ <script src="{{ asset('/argon/assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
  @yield('js')
