28b4cfe60270b73b7247fa85746105bbb32f241a
= <=>
2021-02-23 14:47:31 +0800

    fixes bugs in tenants show page.

--- resources/views/webapp/tenants/show.blade.php
+++ resources/views/webapp/tenants/show.blade.php
@@ -458,7 +458,7 @@
                 @if($pending_balance->count()>0)
                 <a title="proceed to moveout" href="#" data-toggle="modal" data-target="#pendingBalance" class="btn btn-sm btn-primary text-white"><i class="fas fa-sign-out-alt"></i></a>
                 @else
-                  @if($contract->status != 'inactive')
+                  @if($item->status != 'inactive')
                   <a tilt="proceed to moveout" lass="btn btn-success btn-sm text-white" href="/property/{{Session::get('property_id')}}/tenant/{{ $item->tenant_id_foreign }}/contract/{{ $item->contract_id }}/moveout"><i class="fas fa-sign-out-alt"></i>  Moveout</a>
                   @endif
                 @endif
